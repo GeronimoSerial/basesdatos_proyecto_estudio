3b87f04c6668825a4e70bfd71d9f98b8faea4a7a
matiaslago <lagomatias01@hotmail.com>
2025-11-05 10:17:01 -0300

    Procedimiento almacenado. insertar, asignar, borrar problematicas de escuela

--- script/Procedimientos y funciones almacenadas/script.sql
+++ script/Procedimientos y funciones almacenadas/script.sql
@@ -3,7 +3,7 @@
 --UPDATE
 --DELETE
 
---insertar una persona
+--Procedimiento para insertar una persona
 CREATE PROCEDURE insertarPersona
   @DNI INT,
   @Nombre VARCHAR(50),
@@ -17,7 +17,7 @@ BEGIN
 END;
 GO
 
---modificar una persona
+--Procedimiento para modificar una persona
 CREATE PROCEDURE modificarPersona
   @DNI INT,
   @NuevoTelefono NUMERIC(20,0),
@@ -32,7 +32,7 @@ BEGIN
 END;
 GO
 
---borrar una persona
+--Procedimiento para borrar una persona
 CREATE PROCEDURE borrarPersona
   @DNI INT
 AS
@@ -42,7 +42,7 @@ BEGIN
 END;
 GO
 
---insertar una escuela
+--Procedimiento para insertar una escuela
 CREATE PROCEDURE insertarEscuela
   @CUE INT,
   @Nombre VARCHAR(100),
@@ -62,7 +62,7 @@ BEGIN
 END;
 GO
 
---modificar una escuela
+--Procedimiento para modificar una escuela
 CREATE PROCEDURE modificarEscuela
   @CUE INT,
   @NuevoTelefono NUMERIC(20,0),
@@ -77,12 +77,50 @@ BEGIN
 END;
 GO
 
---borrar una escuela
+--Procedimiento para borrar una escuela
 CREATE PROCEDURE borrarEscuela
   @CUE INT
 AS
 BEGIN
   DELETE FROM institucional.escuela
   WHERE cue = @CUE;
+END;
+GO
+
+-- Procedimiento para insertar una problematica
+CREATE PROCEDURE insertarProblematica
+  @Dimension VARCHAR(50),
+  @Descripcion VARCHAR(50)
+AS
+BEGIN
+  INSERT INTO relevamiento.problematica (dimension, descripcion)
+  VALUES (@Dimension, @Descripcion);
+END;
+GO
+
+-- Procedimiento para asignar problematica a una escuela
+CREATE PROCEDURE asignarProblematicaEscuela
+  @IdProblematica INT,
+  @IdEscuela INT
+AS
+BEGIN
+  -- se verifica que no exista la relacion
+  IF NOT EXISTS (SELECT 1 FROM relevamiento.escuela_problematica 
+  WHERE id_problematica = @IdProblematica AND id_escuela = @IdEscuela)
+  BEGIN
+    INSERT INTO relevamiento.escuela_problematica (id_problematica, id_escuela)
+    VALUES (@IdProblematica, @IdEscuela);
+  END
+END;
+GO
+
+-- Procedimiento para eliminar problematica de una escuela
+CREATE PROCEDURE quitarProblematicaEscuela
+  @IdProblematica INT,
+  @IdEscuela INT
+AS
+BEGIN
+  DELETE FROM relevamiento.escuela_problematica
+  WHERE id_problematica = @IdProblematica AND id_escuela = @IdEscuela;
 END;
 GO
